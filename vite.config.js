a2f3b852962b4a0c7ec18e7512d394716d174677
jamesmarin2932 <jamesmarin827@gmail.com>
2024-11-24 17:23:03 -0500

    cambios en nombre facturas

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build:{
-    outdir:'dist'
-  }
+  build: {
+    outDir: 'dist',  // Salida de la carpeta de producción
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Esto hace que '@' apunte a la carpeta src
+    },
+  },
 })
