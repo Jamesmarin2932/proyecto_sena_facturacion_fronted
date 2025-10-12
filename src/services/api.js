13b134966590c9f5b8968804c6d8ef28cebf4458
james marin <jamesmarin827@gmail.com>
2025-10-12 17:22:05 -0500

    cambios archivo api

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',  // Cambia según tu URL base
+  baseURL: import.meta.env.VITE_API_URL, // ✅ Usa la URL según el entorno
 });
 
 // Interceptor para añadir token a todas las peticiones
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
